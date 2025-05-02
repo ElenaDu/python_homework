7fd029f700cb67c033afe8ff7add7e2636b416b8
Alena Dudko <e.v.dudko@gmail.com>
2025-05-01 19:41:13 -0500

    HW 7 in progress...

---
+++ assignment7/sql_intro.py
@@ -0,0 +1,107 @@
+import sqlite3
+
+def add_publisher(cursor, name):
+    try:
+        cursor.execute("INSERT INTO publishers (name) VALUES (?)", (name,))
+    except sqlite3.Error as e:
+        print(f"An error occurred while adding a publisher: {e}")
+
+def add_magazine(cursor, name,publisher_name):
+    try:
+        cursor.execute("SELECT id FROM publishers WHERE name = ?", (publisher_name,))
+        publisher_id = cursor.fetchone()
+        if publisher_id:
+            try:
+                cursor.execute("INSERT INTO magazines (name, publisher_id) VALUES (?,?)", (name, publisher_id[0]))
+            except sqlite3.IntegrityError:
+                print(f"{name} is already in the database.")
+        else:
+            print(f"Publisher '{publisher_name}' not found.")
+    except sqlite3.Error as e:
+        print(f"An error occurred while adding a magazine: {e}")
+
+def add_subscriber(cursor, name, address):
+    try:
+        cursor.execute("SELECT id FROM subscribers WHERE name = ? AND address = ?", (name, address))
+        if cursor.fetchone():
+            print(f"Subscriber '{name}' with '{address}' already exists.")
+        else:
+            cursor.execute("INSERT INTO subscribers (name, address) VALUES (?, ?)", (name, address))
+    except sqlite3.Error as e:
+        print(f"An error occurred while adding a subscriber: {e}")
+
+def add_subscription(cursor, subscriber_name, subscriber_address, magazine_name, expiration_date):
+    try: 
+        cursor.execute("SELECT id FROM subscribers WHERE name = ? AND address = ?", (subscriber_name, subscriber_address))
+        subscriber_result = cursor.fetchone()
+
+        cursor.execute("SELECT id FROM magazines WHERE name = ?", (magazine_name,))
+        magazine_result = cursor.fetchone()
+
+        if subscriber_result and magazine_result:
+            try: 
+                cursor.execute("""
+                    INSERT INTO subscriptions (subscriber_id, magazine_id, expiration_date) 
+                    VALUES (?, ?, ?)""",
+                    (subscriber_result[0], magazine_result[0], expiration_date))
+            except sqlite3.IntegrityError:
+                print(f"{subscriber_name} is already subscribed to '{magazine_name}'.")
+        else:
+            print("Subscriber or magazine not found.")
+    except sqlite3.Error as e:
+        print(f"An error occurred while adding a subscription: {e}") 
+
+#Task 1: Create a New SQLite Database
+try:
+    with sqlite3.connect("../db/magazines.db") as conn:
+        print("Database created and connected successfully.")
+        conn.execute("PRAGMA foreign_keys = 1")
+        cursor = conn.cursor()
+
+        #Task 2: Define Database Structure
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS publishers (
+            id INTEGER PRIMARY KEY,
+            name  TEXT NOT NULL UNIQUE    
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS magazines (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            publisher_id INTEGER NOT NULL,         
+            FOREIGN KEY (publisher_id) REFERENCES publishers (id)
+        )
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS subscribers (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            address TEXT NOT NULL
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS subscriptions (
+            id INTEGER PRIMARY KEY,
+            subscriber_id INTEGER NOT NULL,
+            magazine_id INTEGER NOT NULL,   
+            expiration_date TEXT NOT NULL,          
+            FOREIGN KEY (subscriber_id) REFERENCES subscribers (id),
+            FOREIGN KEY (magazine_id) REFERENCES magazines (id)
+            UNIQUE(subscriber_id, magazine_id)
+        )
+        """)
+        conn.commit()
+        print("Tables created successfully.")
+
+        #Task 3: Populate Tables with Data
+        add_publisher(cursor, "Tax Analysts")
+        add_publisher(cursor, "National Geographic Society")
+        add_publisher(cursor, "Tax Analysts")
+        add_publisher(cursor, "Dell Magazines")
+
+        
+except sqlite3.Error as e:
+    print(f"An error occurred while connecting to the database: {e}")
